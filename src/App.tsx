675b0fad45789360ee9fae8eb7a4f30492b141a2
junKitClover <jkchan.clovers@gmail.com>
2022-07-11 22:35:44 +0800

    update package

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,32 @@ import logo from './logo.svg';
 // @ts-ignore
 import { Button } from 'try-rollup';
 // @ts-ignore
-import { Button as TacklitButton } from '@tacklit/style-guide';
+import { Button as TacklitButton, ThemeProvider, Header } from '@tacklit/style-guide';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button label="from try rollup" primary/>
-        <TacklitButton label="from try tacklit" primary/>
-      </header>
-    </div>
+    <ThemeProvider theme="tacklit">
+      <Header onCreateAccount={()=>{}} onLogin={()=>{}} onLogout={()=>{}}/>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+            <Button label="from try rollup" primary/>
+            <TacklitButton label="from try tacklit" primary/>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
